d0bb4d005359af95a78605c543aa58fe72abce2c
宮田昌平 <heyheyshohey0104@gmail.com>
2018-09-06 23:10:31 +0900

    delete-link-show-recipe-titile
    レシピの取消ボタンを押すとその場で、formを削除して、元の値を出現させる。

--- app/assets/javascripts/recipe-form-edit.js
+++ app/assets/javascripts/recipe-form-edit.js
@@ -2,10 +2,11 @@ $(function(){
   function recipeTitleForm(recipeTitle){
     var html = `<form class="titleform">
                   <input required="required" type="text" id="recipe_title" name="recipe[title]" value="${recipeTitle}">
-                  <button class="editor_ok_button" type="submit">保存</button>`
+                  <button class="editor_ok_button" type="submit">保存</button>
+                  <a href="" class="editor_cancel">取消</a>`
     return html;
 };
-
+// タイトルをクリックするとフォームが現れます。
   $("#edit-recipe-title").on("click", function(){
     var recipeTitle = $("#edit-recipe-title").html();
     $("#edit-recipe-title").hide();
@@ -13,14 +14,19 @@ $(function(){
     $(".recipe-title").append(html);
   });
 
+// 取消リンクを押すと
+  $(document).on("click", ".editor_cancel", function(){
+    $(".titleform").remove();
+    $("#edit-recipe-title").show();
+  });
+
+// 保存ボタンを押すと
   $(document).on("submit", ".titleform", function(e){
     e.preventDefault();
     // var value = $("#recipe_title").val();
-    // var fd = new FormData($('.titleform').get(0));
-    // console.log(fd);
+    // valueではうまくいかなかった。原因を探る必要がありそう
     var formData = new FormData(this);
     var id = Number($(this).parents(".recipe-title").attr("recipeid"));
-    // console.log(value);
     $.ajax({
       url: "/recipes/" + id,
       type: "PATCH",
@@ -29,9 +35,8 @@ $(function(){
       processData: false,
       contentType: false
     })
-    .done(function(data){
-      console.log(data);
-      $("#edit-recipe-title").html(data);
+    .done(function(recipe){
+      $("#edit-recipe-title").html(recipe.title);
       $(".titleform").remove();
       $("#edit-recipe-title").show();
     })
