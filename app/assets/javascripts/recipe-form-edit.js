62fa70849a1d3ec44a0da8b6772fcd44bb59518a
宮田昌平 <heyheyshohey0104@gmail.com>
2018-09-07 15:10:25 +0900

    recipe-catch_copy-ajax
    レシピのキャッチフレーズを非同期化の実装

--- app/assets/javascripts/recipe-form-edit.js
+++ app/assets/javascripts/recipe-form-edit.js
@@ -3,10 +3,21 @@ $(function(){
     var html = `<form class="titleform">
                   <input required="required" type="text" id="recipe_title" name="recipe[title]" value="${recipeTitle}">
                   <button class="editor_ok_button" type="submit">保存</button>
-                  <a href="" class="editor_cancel">取消</a>`
+                  <a href="" class="editor_cancel">取消</a>
+                  </form>`
     return html;
 };
-// タイトルをクリックするとフォームが現れます。
+
+function recipeCatchCopyForm(recipeCatchCopy){
+    var html = `<form class="catch_copy_form">
+                  <textarea class="disable_invalid_character" name="recipe[catch_copy]">${recipeCatchCopy}</textarea>
+                  <br>
+                  <button class="editor_ok_button-catch_copy" type="submit">保存</button>
+                  <a href="" class="editor_cancel-catch_copy">取消</a>
+                </form>`
+    return html;
+  };
+// レシピのタイトルをクリックするとフォームが現れます。
   $("#edit-recipe-title").on("click", function(){
     var recipeTitle = $("#edit-recipe-title").html();
     $("#edit-recipe-title").hide();
@@ -26,7 +37,7 @@ $(function(){
     // var value = $("#recipe_title").val();
     // valueではうまくいかなかった。原因を探る必要がありそう
     var formData = new FormData(this);
-    var id = Number($(this).parents(".recipe-title").attr("recipeid"));
+    var id = Number($(this).parents(".recipe-main").attr("recipeid"));
     $.ajax({
       url: "/recipes/" + id,
       type: "PATCH",
@@ -45,4 +56,53 @@ $(function(){
     })
     return false;
   });
+
+  // レシピのキャッチコピー
+  $(".catchphrase").on("click", function(){
+    var recipeCatchCopy = "";
+    if($(".recipe-catch_copy-edit").length){
+      var recipeCatchCopy = $(".recipe-catch_copy-edit").html();
+    };
+    $(".catchphrase").hide();
+    var html = recipeCatchCopyForm(recipeCatchCopy);
+    $(".catchphrase").before(html);
+  });
+
+// 取消ボタンを押すと
+  $(document).on("click", ".editor_cancel-catch_copy", function(){
+    $(".catch_copy_form").remove();
+    $(".catchphrase").show();
+  });
+
+// 保存ボタンを押すと
+  $(document).on("submit", ".catch_copy_form", function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var id = Number($(this).parents(".recipe-main").attr("recipeid"));
+    $.ajax({
+      url: "/recipes/" + id,
+      type: "PATCH",
+      data: formData,
+      dataType: "json",
+      processData: false,
+      contentType: false
+    })
+    .done(function(recipe){
+      if($(".recipe-catch_copy-edit").length){
+        $(".recipe-catch_copy-edit").html(recipe.catch_copy);
+      }
+      else{
+        $(".catchphrase").append(`<div class="recipe-catch_copy-edit">`);
+        $(".recipe-catch_copy-edit").html(recipe.catch_copy);
+        $(".edit-image").remove();
+        $(".exaple-catch_copy").remove();
+      };
+      $(".catch_copy_form").remove();
+      $(".catchphrase").show();
+    })
+    .fail(function(){
+      alert('error');
+    })
+    return false;
+  })
 });
