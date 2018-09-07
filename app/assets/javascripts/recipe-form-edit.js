397444aed05f13ea3e18fa551ad6b26ca10016e5
宮田昌平 <heyheyshohey0104@gmail.com>
2018-09-07 17:11:13 +0900

    recipe-tips-ajax
    レシピのコツの非同期化の実装

--- app/assets/javascripts/recipe-form-edit.js
+++ app/assets/javascripts/recipe-form-edit.js
@@ -1,4 +1,5 @@
 $(function(){
+  // レシピのタイトル
   function recipeTitleForm(recipeTitle){
     var html = `<form class="titleform">
                   <input required="required" type="text" id="recipe_title" name="recipe[title]" value="${recipeTitle}">
@@ -6,17 +7,8 @@ $(function(){
                   <a href="" class="editor_cancel">取消</a>
                   </form>`
     return html;
-};
-
-function recipeCatchCopyForm(recipeCatchCopy){
-    var html = `<form class="catch_copy_form">
-                  <textarea class="disable_invalid_character" name="recipe[catch_copy]">${recipeCatchCopy}</textarea>
-                  <br>
-                  <button class="editor_ok_button-catch_copy" type="submit">保存</button>
-                  <a href="" class="editor_cancel-catch_copy">取消</a>
-                </form>`
-    return html;
   };
+
 // レシピのタイトルをクリックするとフォームが現れます。
   $("#edit-recipe-title").on("click", function(){
     var recipeTitle = $("#edit-recipe-title").html();
@@ -58,6 +50,16 @@ function recipeCatchCopyForm(recipeCatchCopy){
   });
 
   // レシピのキャッチコピー
+  function recipeCatchCopyForm(recipeCatchCopy){
+    var html = `<form class="catch_copy_form">
+                  <textarea class="disable_invalid_character" name="recipe[catch_copy]">${recipeCatchCopy}</textarea>
+                  <br>
+                  <button class="editor_ok_button-catch_copy" type="submit">保存</button>
+                  <a href="" class="editor_cancel-catch_copy">取消</a>
+                </form>`
+    return html;
+  };
+
   $(".catchphrase").on("click", function(){
     var recipeCatchCopy = "";
     if($(".recipe-catch_copy-edit").length){
@@ -105,4 +107,54 @@ function recipeCatchCopyForm(recipeCatchCopy){
     })
     return false;
   })
+
+// レシピのコツ
+  function recipeTipsForm(recipeTips){
+    var html = `<form class="tips_form">
+                  <textarea class="disable_invalid_character" name="recipe[tips]">${recipeTips}</textarea>
+                  <br>
+                  <button class="editor_ok_button-tips" type="submit">保存</button>
+                  <a href="" class="editor_cancel-tips">取消</a>
+                </form>`
+    return html;
+  };
+
+  $(".recipe-tips-edit").on("click", function(){
+    var recipeTips = "";
+    if($(".recipe-tips-edit").length){
+      var recipeTips = $(".recipe-tips-edit-present").html();
+    };
+    $(".recipe-tips-edit").hide();
+    var html = recipeTipsForm(recipeTips);
+    $(".recipe-tips-edit").before(html);
+  });
+
+  $(document).on("submit", ".tips_form", function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var id = Number($(this).parents(".recipe-main").attr("recipeid"));
+    $.ajax({
+      url: "/recipes/" + id,
+      type: "PATCH",
+      data: formData,
+      dataType: "json",
+      processData: false,
+      contentType: false
+    })
+    .done(function(recipe){
+      if($(".recipe-tips-edit-present").length){
+        $(".recipe-tips-edit-present").html(recipe.tips);
+      }
+      else{
+        $(".catchphrase").append(`<div class="recipe-tips-edit-present">`);
+        $(".recipe-tips-edit-present").html(recipe.tips);
+        $(".example-tips").remove();
+      };
+      $(".tips_form").remove();
+      $(".recipe-tips-edit").show();
+    })
+    .fail(function(recipe){
+      alert('error');
+    });
+  });
 });
