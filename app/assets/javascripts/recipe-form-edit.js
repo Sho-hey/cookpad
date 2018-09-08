44a7f8b4607067c0a82c74f9628705047c60cace
宮田昌平 <heyheyshohey0104@gmail.com>
2018-09-08 15:52:25 +0900

    ajax-recipe-background
    レシピの背景でのajaxの実装を行いました。

--- app/assets/javascripts/recipe-form-edit.js
+++ app/assets/javascripts/recipe-form-edit.js
@@ -8,7 +8,6 @@ $(function(){
                   </form>`
     return html;
   };
-
 // レシピのタイトルをクリックするとフォームが現れます。
   $("#edit-recipe-title").on("click", function(){
     var recipeTitle = $("#edit-recipe-title").html();
@@ -16,14 +15,12 @@ $(function(){
     var html = recipeTitleForm(recipeTitle);
     $(".recipe-title").append(html);
   });
-
 // 取消リンクを押すと
   $(document).on("click", ".editor_cancel", function(){
     $(".titleform").remove();
     $("#edit-recipe-title").show();
     return false;
   });
-
 // 保存ボタンを押すと
   $(document).on("submit", ".titleform", function(e){
     e.preventDefault();
@@ -62,7 +59,6 @@ $(function(){
                 </form>`
     return html;
   };
-
   $(".catchphrase").on("click", function(){
     var recipeCatchCopy = "";
     if($(".recipe-catch_copy-edit").length){
@@ -72,14 +68,12 @@ $(function(){
     var html = recipeCatchCopyForm(recipeCatchCopy);
     $(".catchphrase").before(html);
   });
-
 // 取消ボタンを押すと
   $(document).on("click", ".editor_cancel-catch_copy", function(){
     $(".catch_copy_form").remove();
     $(".catchphrase").show();
     return false;
   });
-
 // 保存ボタンを押すと
   $(document).on("submit", ".catch_copy_form", function(e){
     e.preventDefault();
@@ -124,17 +118,15 @@ $(function(){
                 </form>`
     return html;
   };
-
   $(".recipe-tips-edit").on("click", function(){
     var recipeTips = "";
-    if($(".recipe-tips-edit").length){
+    if($(".recipe-tips-edit-present").length){
       var recipeTips = $(".recipe-tips-edit-present").html();
     };
     $(".recipe-tips-edit").hide();
     var html = recipeTipsForm(recipeTips);
     $(".recipe-tips-edit").before(html);
   });
-
 // 取消ボタン
   $(document).on("click", ".editor_cancel-tips", function(){
     $(".tips_form").remove();
@@ -159,12 +151,70 @@ $(function(){
         $(".recipe-tips-edit-present").html(recipe.tips);
       }
       else{
-        $(".catchphrase").append(`<div class="recipe-tips-edit-present">`);
+        $(".recipe-tips-edit").append(`<div class="recipe-tips-edit-present">`);
         $(".recipe-tips-edit-present").html(recipe.tips);
         $(".example-tips").remove();
+        $(".edit-image").remove();
       };
       $(".tips_form").remove();
       $(".recipe-tips-edit").show();
+    })
+    .fail(function(recipe){
+      alert('error');
+    });
+    return false;
+  });
+
+
+// レシピの生い立ち
+  function recipeBackgroundForm(recipeBackground){
+    var html = `<form class="background_form">
+                  <textarea class="disable_invalid_character" name="recipe[background]" style="width: 290px; height: 100px;">${recipeBackground}</textarea>
+                  <br>
+                  <button class="editor_ok_button-background" type="submit">保存</button>
+                  <a href="" class="editor_cancel-background">取消</a>
+                </form>`
+    return html;
+  };
+  $(".recipe-background-edit").on("click", function(){
+    var recipeBackground = "";
+    if($(".recipe-background-edit-present").length){
+      var recipeBackground = $(".recipe-background-edit-present").html();
+    };
+    $(".recipe-background-edit").hide();
+    var html = recipeBackgroundForm(recipeBackground);
+    $(".recipe-background-edit").before(html);
+  });
+// 取消ボタン
+  $(document).on("click", ".editor_cancel-background", function(){
+    $(".background_form").remove();
+    $(".recipe-background-edit").show();
+    return false;
+  });
+// 保存ボタン
+  $(document).on("submit",".background_form", function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var id = Number($(this).parents(".recipe-main").attr("recipeid"));
+    $.ajax({
+      url: "/recipes/" + id,
+      type: "PATCH",
+      data: formData,
+      dataType: "json",
+      processData: false,
+      contentType: false
+    })
+    .done(function(recipe){
+      if($(".recipe-background-edit-present").length){
+        $(".recipe-background-edit-present").html(recipe.background);
+      }
+      else{
+        $(".recipe-background-edit").append(`<div class="recipe-background-edit-present">`);
+        $(".recipe-background-edit-present").html(recipe.history);
+        $(".recipe-background-edit").remove();
+      };
+      $(".background_form").remove();
+      $(".recipe-background-edit").show();
     })
     .fail(function(recipe){
       alert('error');
